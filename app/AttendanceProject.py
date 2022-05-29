40d9800082da7e6b94f01b7d62fb2e83df6ff7b0
Maida-Iftikhar <maidaiftikhar0@gmail.com>
2022-05-29 10:20:59 +0530

    appcommit

---
+++ app/AttendanceProject.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8844a0d3bb7071345b2365a9213413969ba2645cef82f200af6455227a374f92
+size 2769
---
+++ app/cv2/version.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5441b580070da76c34ef48f29d249bf3516ac3ffa9a9e74e94bddae9d159b02
+size 79
